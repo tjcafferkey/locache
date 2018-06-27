e29f58227c439b276b0893f3dc9fbf0701ba8469
Thomas Roberts <celeissl@gmail.com>
2018-06-27 10:39:39 +0100

    Added a function to allow you to write to session storage as well as local storage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 export function cache(item) {
-  return {
-      get() {
-          return JSON.parse(localStorage.getItem(item));
-      },
+    return {
+        get() {
+            return JSON.parse(localStorage.getItem(item));
+        },
 
-      set(data) {
-          localStorage.setItem(item, JSON.stringify(data));
-      },
+        set(data) {
+            localStorage.setItem(item, JSON.stringify(data));
+        },
 
-      remove() {
-          localStorage.removeItem(item);
-      }
-  };
+        remove() {
+            localStorage.removeItem(item);
+        }
+    };
+}
+
+export function sessionCache(item) {
+    return {
+        get() {
+            return JSON.parse(sessionStorage.getItem(item));
+        },
+
+        set(data) {
+            sessionStorage.setItem(item, JSON.stringify(data));
+        },
+
+        remove() {
+            sessionStorage.removeItem(item);
+        }
+    };
 }
